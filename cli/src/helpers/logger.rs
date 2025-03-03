0054b1a4adb444489861098532a7344e5febade2
Konstantin Pandl <konstantinpandl@yahoo.de>
2025-03-03 16:16:06 +0100

    v320

--- cli/src/helpers/logger.rs
+++ cli/src/helpers/logger.rs
@@ -160,6 +160,6 @@ pub fn welcome_message() -> String {
 "#
     .white()
     .bold();
-    output += &"👋 Welcome to Aleo! We thank you for running a node and supporting privacy.\n".bold();
+    output += &"👋 Welcome to Aleo v3.2.0! We thank you for running a node and supporting privacy.\n".bold();
     output
 }
