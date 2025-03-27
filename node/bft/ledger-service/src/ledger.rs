26885aed0de453ea372ee53a1ba94d3213b22cc8
Konstantin Pandl <konstantinpandl@yahoo.de>
2025-03-27 15:21:06 +0100

    change

--- node/bft/ledger-service/src/ledger.rs
+++ node/bft/ledger-service/src/ledger.rs
@@ -375,7 +375,7 @@ impl<N: Network, C: ConsensusStorage<N>> LedgerService<N> for CoreLedgerService<
             metrics::update_block_metrics(block);
         }
 
-        tracing::info!("\n\nAdvanced to block {} at round {} - {}\n", block.height(), block.round(), block.hash());
+        tracing::info!("\n\nAdvanced1 to block {} at round {} - {}\n", block.height(), block.round(), block.hash());
         Ok(())
     }
 
